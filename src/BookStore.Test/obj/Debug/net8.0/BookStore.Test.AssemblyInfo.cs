d1726e0028db0365974c096667ce9f0a186ecf86
lam <lin.jing.peng@qq.com>
2025-03-14 17:38:31 +0800

    modify（完善readme文件）完善readme文件

--- src/BookStore.Test/obj/Debug/net8.0/BookStore.Test.AssemblyInfo.cs
+++ src/BookStore.Test/obj/Debug/net8.0/BookStore.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f958c9700c5ecb7676ae0cdc751a780c0117223")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f29c99482dc41e64c024963a41baedeb19ef95c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
